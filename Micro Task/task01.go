827b593e907a692d298bc452b1b89fde9c46e68b
0Fardin10 <fardinmd268@gmail.com>
2026-02-24 23:43:50 +0600

    thats for today , i have exam

--- Micro Task/task01.go
+++ Micro Task/task01.go
@@ -1,1 +1,12 @@
 package main
+import "fmt"
+func ohlog() string {
+var quill string = `He said "Go is awesome"`
+return quill
+}
+func main(){
+fmt.Println(ohlog())
+}
+
+
+// output like that  = He said "Go is awesome"
