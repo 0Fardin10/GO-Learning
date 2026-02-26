2565f19df41c6358c8232832ce9afba8a4e0e146
0Fardin10 <fardinmd268@gmail.com>
2026-02-26 21:07:58 +0600

    solving

--- Micro Task/task04.go
+++ Micro Task/task04.go
@@ -1,1 +1,11 @@
+//output will be C:\Program Files\Go
 package main
+import "fmt"
+func main(){
+ fmt.Println(path())
+}
+func path()string{
+ var path string = `C:\Program Files\Go`
+ return  path
+}
+
