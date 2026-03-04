054f16658ed34407399e63975900a9bd2942edbb
0Fardin10 <fardinmd268@gmail.com>
2026-03-04 23:35:21 +0600

    task01

---
+++ Array & Slices/mini project/01.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func findSum(num [5]int) int {
+	sum := 0
+	for i := 0; i < len(num); i++ {
+		sum += num[i]
+	}
+	return sum
+}
+
+func main() {
+	var num [5]int
+	fmt.Println("Enter 5 number :")
+	for i := 0; i < 5; i++ {
+		fmt.Scan(&num[i])
+	}
+	fmt.Println("Your number is", num)
+	result := findSum(num)
+	fmt.Println("Here the sum is :", result)
+}
