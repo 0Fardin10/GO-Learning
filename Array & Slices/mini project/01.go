bf53d309ff585eb9c7b1092cb995e9aee7281ae2
0Fardin10 <fardinmd268@gmail.com>
2026-03-06 00:18:11 +0600

    ..

--- Array & Slices/mini project/01.go
+++ Array & Slices/mini project/01.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+func findMini(n [5]int) int {
+	mini := n[0]
+	for i := 1; i < 5; i++ {
+		if n[i] < mini {
+			mini = n[i]
+		}
+	}
+	return mini
+}
+
+func findMax(a [5]int) int {
+	max := a[0]
+	for i := 1; i < 5; i++ {
+		if a[i] > max {
+			max = a[i]
+		}
+	}
+	return max
+}
+
 func findSum(num [5]int) int {
 	sum := 0
 	for i := 0; i < len(num); i++ {
@@ -9,14 +29,26 @@ func findSum(num [5]int) int {
 	}
 	return sum
 }
-func 
+
 func main() {
+
 	var num [5]int
-	fmt.Println("Enter 5 number :")
+
+	fmt.Println("Enter 5 numbers:")
+
 	for i := 0; i < 5; i++ {
+		fmt.Printf("Enter number %d: ", i+1)
 		fmt.Scan(&num[i])
 	}
-	fmt.Println("Your number is", num)
-	result := findSum(num)
-	fmt.Println("Here the sum is :", result)
+
+	fmt.Println("Your numbers:", num)
+
+	sum := findSum(num)
+	fmt.Println("Sum:", sum)
+
+	maxValue := findMax(num)
+	fmt.Println("Biggest number:", maxValue)
+
+	minValue := findMini(num)
+	fmt.Println("Smallest number:", minValue)
 }
