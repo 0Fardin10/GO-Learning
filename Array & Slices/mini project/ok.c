a7ed90a2a04bd3e6a63971708ae0f1b70c5ccc58
0Fardin10 <fardinmd268@gmail.com>
2026-03-25 20:38:14 +0600

    back

--- Array & Slices/mini project/ok.c
+++ Array & Slices/mini project/ok.c
@@ -1,3 +1,24 @@
 //Array Reverse (Loop + Array) taking 5 user input then reverse
 #include<stdio.h>
-int
+int main(){
+    int a[5];
+    printf("enter 5 number:\n");
+    for (int i = 0; i < 5; i++)
+    {
+    scanf("%d",&a[i]);
+    printf("write :%d\n",a[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("you entered %d\n",a[i]);
+    }
+
+    
+printf("now reverse time\n");
+for (int  i = 4; i>=0; i--)
+{
+    printf("%d\n",a[i]);
+}
+return 0;
+
+}
